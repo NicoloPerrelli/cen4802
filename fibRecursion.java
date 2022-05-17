d2ecd9482ae4dcda376059d442c4635bd9fbcf6d
NicoloPerrelli <nicoloperrelli@gmail.com>
2022-05-17 12:42:28 -0400

    added docs and fixed file/class name convention

--- fibRecursion.java
+++ fibRecursion.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class fibRecursion {
+public class FibRecursion {
 	public static void main(String[] args) throws Exception {
 		  
 		//input
@@ -8,6 +8,8 @@ public class fibRecursion {
 		System.out.println("Enter n'th fib sequence");
 
 		String userInput = myObj.nextLine();  //user input
+        
+        //some filter here for proper inputs
 
 		int count = Integer.parseInt(userInput);
 
@@ -16,6 +18,12 @@ public class fibRecursion {
 		myObj.close();
 	}
 
+    /**
+     * Allows one to enter a whole number and get returned a position from the Fibonacci sequence using a recursive method.
+     * @param count (int) as a dynamic selection for position
+     * @return The nth position in the Fibonacci sequence as a long
+     * @author Nicolo Perrelli
+     */
 	public static long fibRecursive(int count){
 		if (count <= 1) {return count;}
 		return fibRecursive(count - 1) + fibRecursive(count - 2);
